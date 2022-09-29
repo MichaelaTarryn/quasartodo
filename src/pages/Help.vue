6a510deb751192304b453d9a912f49c937e959db
MichaelaTarryn <mward7006@gmail.com>
2022-09-29 15:04:57 +0200

    first

---
+++ src/pages/Help.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">
+      Help
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis a sapiente doloremque quisquam minus dicta distinctio delectus dolorum nam sequi, eum eos quibusdam laudantium
+        deleniti aliquid quod enim ab in.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde temporibus eligendi tempore magni veniam recusandae eius error totam aut possimus in molestiae, veritatis omnis
+           vitae excepturi fuga consectetur dicta ullam!</p>
+    </h5>
+  </q-page>
+</template>
+
+<script>
+
+
+export default({
+
+})
+</script>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-page class="bg-grey-3 column" >
+    <q-list class="bg-white" separator bordered>
+      <q-item v-for="(task,index) in tasks" :key="task.title"
+       @click="task.done=!task.done"
+        clickable
+        :class="{'done bg-blue-done':task.done}"
+        v-ripple>
+        <q-item-section avatar>
+          <q-checkbox
+          v-model="task.done"
+          class="no-pointer-events"
+            color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{task.title}}</q-item-label>
+        </q-item-section>
+        <q-item-section
+        v-if="task.done" side>
+        <q-btn
+        flat
+        round
+        dense
+        color="primary"
+         icon="delete" />
+        </q-item-section>
+      </q-item>
+
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+
+
+export default({
+data(){
+  return{
+    tasks:[
+    {
+      title:'Get up',
+      done:false
+    },
+    {
+      title:'check phone',
+      done:false
+    },
+    {
+      title:'call meagan',
+      done:false
+    }
+  ]
+  }
+}
+})
+</script>
+<style lang="scss">
+.q-item__label{
+  text-decoration: line-through;
+  color:#bbb
+}
+</style>
